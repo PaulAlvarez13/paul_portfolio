25ac37c2a1f8f5bb626582b2315b4a0a4f106a94
PaulAlvarez13 <101508702+PaulAlvarez13@users.noreply.github.com>
2023-04-22 18:41:57 +0800

    initial and final upload of files from vsco

---
+++ js/smallscreen.js
@@ -0,0 +1,10 @@
+//Side Menu - For Small Screens (for Responsiveness)
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemeu.style.right = "0";
+}
+function closemenu(){
+  sidemeu.style.right = "-200px";
+}
